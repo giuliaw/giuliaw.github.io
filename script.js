bb0553a72fd6283223b9fc4ca1a6f49bb5834667
giuliaw <59878253+giuliaw@users.noreply.github.com>
2020-05-03 14:10:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function myFunction() {
     }
   }
 
+/* Funktion für das Wetter von weatherwidget.io */ 
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
 
 function myweatherFunction(id) {
@@ -18,10 +19,12 @@ var x = document.getElementById(id);
   }  
 }
 
+/* Initialisierung beim Laden der Seite
+Weather Widget nicht anzeigen */
 function myload () {
   var x1 = document.getElementById("cali-weather-id");
   var x2 = document.getElementById("torquay-weather-id");
 
-  /* x1.style.display = "none";
-  x2.style.display = "none"; */
+  x1.style.display = "none";
+  x2.style.display = "none";
 }
