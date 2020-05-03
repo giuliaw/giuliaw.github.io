aa335d890fd08c527080ab932774747a9799f15c
giuliaw <59878253+giuliaw@users.noreply.github.com>
2020-05-03 13:23:53 +0200

    update

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+
+function myweatherFunction() {
+var x = document.getElementById("weatherwidget-id");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }  
+}
