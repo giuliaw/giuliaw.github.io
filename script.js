0b351dc2bc6a75732421edbcaac0e61e257ff70c
giuliaw <59878253+giuliaw@users.noreply.github.com>
2020-05-14 15:55:59 +0200

    update

--- script.js
+++ script.js
@@ -41,20 +41,7 @@ function myload () {
   x8.style.display = "none";
 }
 
-var btn = $('#topbutton');
 
-$(window).scroll(function() {
-  if ($(window).scrollTop() > 300) {
-    btn.addClass('show');
-  } else {
-    btn.removeClass('show');
-  }
-});
-
-btn.on('click', function(e) {
-  e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '300');
-});
 
 
 
